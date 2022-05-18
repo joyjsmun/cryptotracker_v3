abeda48d1ef5c03c0499f61e836bb20971ded36f
joyjsmun <joy.js.mun@gmail.com>
2022-05-19 00:23:00 +0900

    ADD : coinlist styling

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,20 +1,49 @@
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 import styled from "styled-components"
 
 
 interface ICoins{
     name:string
+    id:string
 }
 
-const Container = styled.div``
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`
+const Header = styled.header`
+    height: 100px;
+    color: #ffffcd;
+    font-weight: 700;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
 
-const Header = styled.header``
+const Title = styled.h1`
+    font-size: x-large;
+    text-align: center;
+`
 
-const Title = styled.h1``
+const CoinList = styled.div``
 
-const CoinList = styled.ul``
-
-const Coin = styled.li``
+const Coin = styled.li`
+   color: #f65555;
+    list-style-type: none;
+    height: 50px;
+    background-color: #ffffcd;
+    margin: 10px;
+    display: flex;
+    align-items: center;
+    padding-left: 20px;
+    border-radius: 15px;
+    :hover{
+        background-color: yellow;
+        transition: 0.5s linear;
+    }
+`
 
  function Coins(){
      const [coins,setCoins] = useState<ICoins[]>([])
@@ -31,7 +60,7 @@ const Coin = styled.li``
             <Title>Coin Tracker v3</Title>
         </Header>
         <CoinList>
-            {coins.map(coin => <Coin>{coin.name}</Coin>)}
+            {coins.map(coin => <Link to={`/${coin.id}`}><Coin>{coin.name} &rarr;</Coin></Link>)}
         </CoinList>
     </Container>
     )
